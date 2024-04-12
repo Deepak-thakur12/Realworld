c5ef423c1dd152040f9b5e286a107be03a0964eb
“Deepak <thakur.lipton14@gmail.com>
2024-04-12 16:41:15 +0530

    for third commit may be

--- app.js
+++ app.js
@@ -2,6 +2,14 @@ function App(){
     return(
         <duv>
             <h1>Hello js</h1>
+            <div>
+                <h1>welcome to js file</h1>
+                <h1>we will design a layout</h1>
+                <h1>now we will play a game here</h1>
+                <h1>lets play game</h1>
+                
+
+            </div>
         </duv>
     )
 }
